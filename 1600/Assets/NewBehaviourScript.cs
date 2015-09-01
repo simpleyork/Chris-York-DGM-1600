8f128e75ccdd52cd2c386ab48258665eabcbc615
Chris York <simpleyork@gmail.com>
2015-09-01 11:34:57 -0600

    1600 Variables

---
+++ 1600/Assets/NewBehaviourScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+	int myInt = 5;
+
+		// Use this for initialization
+	void Start (){
+		myInt = MuliplyByTwo(myInt);
+		Debug.Log (myInt);
+	}
+	int MuliplyByTwo(int number){
+		int ret;
+
+		ret = number * 2;
+
+		return ret;
+	}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
