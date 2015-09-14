e77146fa305dc8ccb5a70e7dcfc3da9bdda470ef
Christopher York <10023539@CS518-02.ad.uvu.edu>
2015-09-14 15:04:57 -0600

    FlyAway

---
+++ Chris_York_DGM_Unity/DGM1600_ChrisYork/DGM1600_ChrisYork/Assets/Script/flyScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class flyScript : MonoBehaviour {
+
+	private float heightTest = 3f;
+
+	void Start () {
+		Debug.Log(transform);
+			if(transform.position.y <= heightTest)
+		{
+			Debug.Log("I'm about to hit the ground!");
+			FlyAway();
+		}
+		else if(transform.position.y >= heightTest) {
+			Debug.Log("I'm flying!");
+		}
+	}
+	private void FlyAway() {
+		Debug.Log ("Fly Away!");
+		Vector3 newPosition = new Vector3(0,7,0);
+		transform.position = newPosition;
+
+	}
+}
